3b892332a95c31f33651b444cb74eb8508133ffb
Jason <2923280915@qq.com>
2023-07-13 16:21:49 +0800

    修改暴露端口；修改swagger配置；

--- MorningStar.Api/Program.cs
+++ MorningStar.Api/Program.cs
@@ -1,9 +1,25 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 #region 将服务添加到容器中
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "晨星博客 接口文档",
+        Description = "晨星博客 - 卢杰晟的个人博客",
+        Contact = new OpenApiContact
+        {
+            Name = "Lenceas",
+            Url = new Uri("https://lujiesheng.cn/")
+        }
+    });
+});
+builder.WebHost.UseUrls("http://*:8079");
 #endregion
 
 var app = builder.Build();
@@ -12,7 +28,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
